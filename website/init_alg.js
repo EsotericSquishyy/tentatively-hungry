fc205e663657b2e6b3aa92fb175d6d3206803e7d
Nate Annau <nateannau@gmail.com>
2024-01-13 21:34:45 -0800

    Swap from fs to d3

--- website/init_alg.js
+++ website/init_alg.js
@@ -1,25 +1,27 @@
-const fs = require('fs');
+d3.csv('../Datasets/genRecipes.csv', (data) => {
+    console.log(data);
 
-const ingMap = {}; // Maps ingredients to a list of recipes it is included in
-const totMap = {}; // Maps recipes to the number of ingredients required
+    const ingMap = {}; // Maps ingredients to a list of recipes it is included in
+    const totMap = {}; // Maps recipes to the number of ingredients required
 
-// Read the CSV file
-const data = fs.readFileSync('../Datasets/genRecipes.csv', 'utf-8');
-const rows = data.split('\n');
-const headers = rows[0].split(',');
+    // Read the CSV file
 
-for (let i = 1; i < rows.length; i++) {
-    const row = rows[i].split(',');
-    let count = 0;
+    const rows = data.split('\n');
+    const headers = rows[0].split(',');
 
-    for (const item of row.slice(headers.indexOf('Recipe'))) {
-        const ingredient = item.replace('[', '').replace(']', '').replace('\'', '').trim().replace('"', '');
+    for (let i = 1; i < rows.length; i++) {
+        const row = rows[i].split(',');
+        let count = 0;
 
-        if (!ingMap[ingredient]) {
-            ingMap[ingredient] = [];
+        for (const item of row.slice(headers.indexOf('Recipe'))) {
+            const ingredient = item.replace('[', '').replace(']', '').replace('\'', '').trim().replace('"', '');
+
+            if (!ingMap[ingredient]) {
+                ingMap[ingredient] = [];
+            }
+            ingMap[ingredient].push(row[headers.indexOf('Recipe')]);
+            count++;
         }
-        ingMap[ingredient].push(row[headers.indexOf('Recipe')]);
-        count++;
+        totMap[row[headers.indexOf('Recipe')]] = count;
     }
-    totMap[row[headers.indexOf('Recipe')]] = count;
-}
+});
