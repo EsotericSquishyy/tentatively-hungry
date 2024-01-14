d393260b7ca1983ea5fbc3f4492c7d687eacbfb6
EsotericSquishyy <96682689+EsotericSquishyy@users.noreply.github.com>
2024-01-13 19:48:04 -0800

    Fixed js algorithm

--- website/alg.js
+++ website/alg.js
@@ -1,19 +1,23 @@
 const fs = require('fs');
 
+const user_ing = ['Banana']
+const user_rat = 0.5
+
 const Ing_Rec = {}; // Maps ingredients to a list of recipes it is included in
 const Rec_Tot = {}; // Maps recipes to the number of ingredients required
+
+
 
 // Read the CSV file
 const data = fs.readFileSync('../Datasets/genRecipes.csv', 'utf-8');
 const rows = data.split('\n');
 const headers = rows[0].split(',');
 
-
 for (let i = 1; i < rows.length; i++) {
     const row = rows[i].split(',');
     let count = 0;
 
-    for (const item of row.slice(headers.indexOf('Ingredients'))) {
+    for (const item of row.slice(headers.indexOf('Recipe'))) {
         const ingredient = item.replace('[', '').replace(']', '').replace('\'', '').trim().replace('"', '');
 
         if (!Ing_Rec[ingredient]) {
@@ -25,7 +29,8 @@ for (let i = 1; i < rows.length; i++) {
     Rec_Tot[row[headers.indexOf('Recipe')]] = count;
 }
 
-const user_ing = ['Pasta', 'Ground Beef', 'Tomato Sauce', 'Onion', 'Garlic', 'Olive Oil', 'Salt', 'Pepper', 'Cabbage Leaves']; // Testing list
+
+
 const Rec_Num = {}; // Maps recipes to the number of ingredients the user has
 
 for (const ingredient of user_ing) {
@@ -39,6 +44,7 @@ for (const ingredient of user_ing) {
 }
 
 
+
 const Rat_Rec = {}; // Maps the rating of the recipe to recipes
 
 for (const recipe in Rec_Num) {
@@ -50,8 +56,21 @@ for (const recipe in Rec_Num) {
     Rat_Rec[rating].push(recipe);
 }
 
+
+
 const keys = Object.keys(Rat_Rec).sort((a, b) => b - a);
 
-for (const key of keys) {
+/*for (const key of keys) {
     console.log(key, Rat_Rec[key]);
+}*/
+
+const output = []
+for (const key of keys){
+	if (key < user_rat)
+		continue
+	for (const recipe of Rat_Rec[key]){
+		output.push(recipe)
+	}
 }
+
+// console.log(output)
